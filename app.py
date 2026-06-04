38a94e2f3b9615f2bf6e866d25cfc7e8351b6106
Andisheh81 <applezangene@gmail.com>
2026-06-04 22:53:17 +0300

    file modifications

--- app.py
+++ app.py
@@ -1,15 +1,7 @@
 import os
 
-target_folder = ".."
+start="../TextFiles/test.txt"
+final="../test.txt"
 
-if os.path.exists(target_folder):
-    # os.listdir returns a list like ['app.py', '.env']
-    all_files = os.listdir(target_folder)
-    
-    print("\n--- Scanning Workspace ---")
-    # We loop through the list one file at a time
-    for file_name in all_files:
-        print(f"Found file: {file_name}")
-        
-else:
-    print("Error: That folder does not exist.")
+if os.path.exists(start):
+    os.rename(start,final)
